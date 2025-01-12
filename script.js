f5e3a4c303ab5d3f1a1ade22e21a3e6d7232a2b3
rightbear <jjj17041113@gmail.com>
2025-01-12 15:57:15 +0800

    feat: redeign funtions for events and add comments
    Build functions for different button events to increase the readibility, and add comments to explain all function features

--- script.js
+++ script.js
@@ -59,6 +59,7 @@ function playGame(playerSelection){
     return (`Round ${round}: ${playRound(humanSelection, computerSelection)}`);
 }
 
+
 let humanScore = 0;
 let computerScore  = 0;
 let round= 0;
@@ -67,43 +68,83 @@ console.log('It\'s the game time');
 const buttons = document.querySelector('#buttons');
 const resultMessage = document.querySelector('#textResult');
 
-// Add event listeners buttons 
+// Remove 'Start' or 'Next' button, them add opions buttons and reminder message
+function enterNewRound(target){
+    buttons.removeChild(target);
+        
+    const rockButton = document.createElement('button');
+    rockButton.textContent = "Rock";
+    rockButton.setAttribute("id", "rock");
 
+    const paperButton = document.createElement('button');
+    paperButton.textContent = "Paper";
+    paperButton.setAttribute("id", "paper");
+
+    const sciButton = document.createElement('button');
+    sciButton.textContent = "Scissors";
+    sciButton.setAttribute("id", "scissors");
+
+    buttons.appendChild(rockButton);
+    buttons.appendChild(paperButton);
+    buttons.appendChild(sciButton);
+
+    resultMessage.textContent = "Select an option."
+}
+
+// Remove 'Restart' button, them add 'Start' buttons and reminder message
+function resetAllGame(target){
+    buttons.removeChild(target);
+    const startButton = document.createElement('button');
+    startButton.textContent = "Start";
+    startButton.setAttribute("id", "start");
+    buttons.appendChild(startButton);
+
+    resultMessage.textContent = "It's game time!";
+}
+
+// Remove option buttons, them add 'Reset' buttons and reminder message
+function summarizeAllGame(){
+    resultMessage.innerHTML += '<p>Game Over:</p>';
+    resultMessage.innerHTML += `<p>Human Score is ${humanScore}<br>Computer Score is ${computerScore}</p>`;
+
+    if(humanScore > computerScore){
+        resultMessage.innerHTML +=("<p>Player is the final winner</p>");
+    }
+    else if(humanScore < computerScore){
+        resultMessage.innerHTML +=("<p>Computer is the final winner</p>");
+    }
+
+    const resetButton = document.createElement('button');
+    resetButton.textContent = "Reset";
+    resetButton.setAttribute("id", "reset");
+
+    buttons.appendChild(resetButton);
+}
+
+// Remove option buttons, them add 'Next' buttons and reminder message
+function summarizeCurrentRound(){
+    const nextButton = document.createElement('button');
+    nextButton.textContent = "Next Round";
+    nextButton.setAttribute("id", "next");
+    buttons.appendChild(nextButton);
+
+    resultMessage.innerHTML += `<p>Human Score is ${humanScore}<br>Computer Score is ${computerScore}</p>`;
+}
+
+// Add event listeners to buttons 
 buttons.addEventListener('click', function ModifySelection(event){
     let target = event.target;
     let playerSelection ;
 
-    // If 'Start' button is clicked, substitued it with option button
+    // If the button in 'Start' or 'Next' page is clicked, replace it with options page
     if(target.id == 'start' || target.id == 'next'){
-        buttons.removeChild(target);
-        
-        const rockButton = document.createElement('button');
-        rockButton.textContent = "Rock";
-        rockButton.setAttribute("id", "rock");
-
-        const paperButton = document.createElement('button');
-        paperButton.textContent = "Paper";
-        paperButton.setAttribute("id", "paper");
-
-        const sciButton = document.createElement('button');
-        sciButton.textContent = "Scissors";
-        sciButton.setAttribute("id", "scissors");
-
-        buttons.appendChild(rockButton);
-        buttons.appendChild(paperButton);
-        buttons.appendChild(sciButton);
-
-        resultMessage.textContent = "Select an option."
+        enterNewRound(target);
     }
+    // If the button in 'Reset' page is clicked, replace it with 'Start' page
     else if(target.id == 'reset'){
-        buttons.removeChild(target);
-        const startButton = document.createElement('button');
-        startButton.textContent = "Start";
-        startButton.setAttribute("id", "start");
-        buttons.appendChild(startButton);
-
-        resultMessage.textContent = "It's game time!";
+        resetAllGame(target);
     }
+    // If buttons in options page is clicked, replace it with 'Next' or 'Restart' page
     else if(target.id == 'rock' || target.id == 'paper' || target.id == 'scissors'){
         // Every button symbol different choice made by player in the game
         switch(target.id) {
@@ -121,34 +162,18 @@ buttons.addEventListener('click', function ModifySelection(event){
         // After clicking the option buttons, the game will starts
         resultMessage.innerHTML = `${playGame(playerSelection)}`;
 
+        // Remove all option buttons(Rock, Paper, Scissors)
         while (buttons.firstChild) {
             buttons.removeChild(buttons.firstChild);
         }
 
+        // If one of the curret players reach 5 points, replace it with 'Restart' page 
         if (humanScore == 5 || computerScore == 5){
-            resultMessage.innerHTML += '<p>Game Over:</p>';
-            resultMessage.innerHTML += `<p>Human Score is ${humanScore}<br>Computer Score is ${computerScore}</p>`;
-        
-            if(humanScore > computerScore){
-                resultMessage.innerHTML +=("<p>Player is the final winner</p>");
-            }
-            else if(humanScore < computerScore){
-                resultMessage.innerHTML +=("<p>Computer is the final winner</p>");
-            }
-        
-            const resetButton = document.createElement('button');
-            resetButton.textContent = "Reset";
-            resetButton.setAttribute("id", "reset");
-
-            buttons.appendChild(resetButton);
+            summarizeAllGame();
         }
+        // If none of the curret players reach 5 points, replace it with 'Next' page 
         else{
-            const nextButton = document.createElement('button');
-            nextButton.textContent = "Next Round";
-            nextButton.setAttribute("id", "next");
-            buttons.appendChild(nextButton);
-
-            resultMessage.innerHTML += `<p>Human Score is ${humanScore}<br>Computer Score is ${computerScore}</p>`;
+            summarizeCurrentRound();
         }
     }
 });
